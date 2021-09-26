a5de4f271d4ca9ee8d7ad1832c934e8d9aa041c0
Mohammad9448 <mohammad.ezz.saker@gmail.com>
2021-09-27 01:31:00 +0200

    First Commit

---
+++ src/components/AllUsers/AllUsers.js
@@ -0,0 +1,33 @@
+import React from "react";
+import './AllUsers.css';
+function AllUsers(props){
+return (
+
+    <table>
+        <thead>
+        <tr>
+            <td>id</td>
+            <td>name</td>
+            <td>age</td>
+            <td>gender</td>
+        </tr>
+        </thead>
+        <tbody>
+    {props.users.map(x=>{
+        return (
+            <tr key={x.id}>
+                <td>{x.id}</td>
+                <td>{x.name}</td>
+                <td>{x.age}</td>
+                <td>{x.gender}</td>
+                <td><button onClick={}></button></td>
+            </tr>
+            
+
+        );
+    })}
+    </tbody>
+    </table>
+);
+}
+export default AllUsers;
